c44cfc47356030b1925c92f1dd2aadddebb9fa1b
kwonshzzang <kwonshzzang@gmail.com>
2025-12-11 14:31:49 +0900

    First Commits

---
+++ src/kr/co/kwonshzzang/designpattern/iterator/Aggregate.java
@@ -0,0 +1,5 @@
+package kr.co.kwonshzzang.designpattern.iterator;
+
+public interface Aggregate {
+    public abstract Iterator iterator();
+}
---
+++ src/kr/co/kwonshzzang/designpattern/iterator/Iterator.java
@@ -0,0 +1,6 @@
+package kr.co.kwonshzzang.designpattern.iterator;
+
+public interface Iterator {
+     public abstract boolean hasNext();
+     public abstract Object next();
+}
